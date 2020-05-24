47945c564cc35c6f9f2d2948bfdea09b9a35d841
Damion M. Johnson <d.m.johnson300@gmail.com>
2020-05-23 23:32:41 -0400

    Created tkinter_hello_prac.py

---
+++ tkinter_hello_prac.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+# Creating a label.
+myLabel = Label(root, text="Hello World!")
+
+# Putting it on the screen.
+myLabel.pack()
+
+# Making the root the main loop.
+root.mainloop()
