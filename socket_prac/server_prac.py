d571fbf40492de5d733447b333f657bc97f449d0
Damion M. Johnson <64974869+KingPin300@users.noreply.github.com>
2020-06-10 22:23:21 -0400

    Add files via upload

---
+++ socket_prac/server_prac.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+
+
+PORT = 5050
+SERVER = "192.168.1.39" # my mothership ip
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+DISCONNECT_MESSAGE = "EXIT"
+HEADER = 64
+FORMAT = 'utf-8'
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handleClient(connection, addr):
+	print(f"[SERVER] {addr} connected.")
+	connected = True
+	while connected:
+		msg_length = connection.recv(HEADER).decode(FORMAT)
+		if msg_length:
+			msg_length = int(msg_length)
+			msg = connection.recv(msg_length).decode(FORMAT)
+			if msg == DISCONNECT_MESSAGE:
+				connected = False
+			else:
+				print(f"[{addr}] {msg}")
+	print(f"[{addr}] has left the server.")
+	connection.close()
+
+
+def start():
+	server.listen()
+	while True:
+		connection, addr = server.accept()
+		thread = threading.Thread(target=handleClient, args=(connection, addr))
+		thread.start()
+		print(f"[SERVER] active thread count {threading.activeCount() - 1 }.")
+
+
+print(f"[SERVER] starting up on {ADDR[0]} port {ADDR[1]}.")
+start()
